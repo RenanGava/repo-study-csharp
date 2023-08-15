61bd5ecc07a59c791b59eaa3ab95a952e494fdbb
RenanGava <renandellecrodegava7040@gmail.com>
2023-08-14 21:18:02 -0300

    .

---
+++ 2-Aulas-Classes-C#/01-Construtores/Produto.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+
+namespace Course{
+
+    class Produto{
+
+        public string Nome = "";
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque(){
+            return Preco * Quantidade;
+        }
+
+        //oq vamos aprender aqui é sobreposiçào que será explicado melhora mais para frente
+        // do curso usando a palavra override
+        public override string ToString()
+        {   // o base é referido a nossa classe Produto ou qualquer
+            // outra classe que fizermos isso. 
+            return Nome 
+            + ", " 
+            +  "$ " 
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " Uninades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public void AddProdutoQuantidade(int addQuant){
+            Quantidade += addQuant;
+        }
+        public void RemoveProduto(int rmQuant){
+            Quantidade -= rmQuant;
+        }
+    }
+}
