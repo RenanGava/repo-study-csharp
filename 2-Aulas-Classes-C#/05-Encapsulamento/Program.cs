7accf0af9f908b18bb1167d926cadd2091d31446
RenanGava <renandellecrodegava7040@gmail.com>
2023-08-15 08:50:07 -0300

    .

---
+++ 2-Aulas-Classes-C#/05-Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Course{
+
+    class Program{
+
+        static void Main(string[] args){
+            
+            Produto p = new Produto("TV", 500.00, 10);
+            Console.WriteLine(p.GetNome());
+            p.SetNome(nome:"Cv"); // mudamos o nome do parametro usando este método
+            Console.WriteLine(p.GetNome(), p.GetPreco());
+            
+        }
+    }
+}
