9f3a6312be640e1ce18c069d7333da4061b26016
RenanGava <renandellecrodegava7040@gmail.com>
2023-08-22 08:41:54 -0300

    vetores

---
+++ 3-Aulas-Memorias-Array-List/03-Vetores-02/Product.cs
@@ -0,0 +1,8 @@
+namespace Course{
+
+    class Product{
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ 3-Aulas-Memorias-Array-List/03-Vetores-02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Name = name, Price = price };
+
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < 0; i++){
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine("AVARAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
