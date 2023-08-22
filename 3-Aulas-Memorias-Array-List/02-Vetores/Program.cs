710a3e63092e05cc5e9ba69c59ed929e6449856d
RenanGava <renandellecrodegava7040@gmail.com>
2023-08-22 08:42:36 -0300

    aulas 3

---
+++ 3-Aulas-Memorias-Array-List/02-Vetores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Console.WriteLine("Digite um Número: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite ua Altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n ; i++){
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine(vect[i]);
+            }
+
+            
+        
+        }
+    }
+    
+}
